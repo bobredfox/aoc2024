de709fed7bb7d9c462427d08547febff8438f640
bobredfox <gelberwolf@posteo.de>
2024-12-06 21:50:16 +0100

    Day06 rustfmt

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
-use std::thread;
 use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
 
 #[derive(Clone)]
 struct Board {
@@ -10,7 +10,7 @@ struct Board {
     guard: Position,
     guard_direction: Direction,
     guard_visited_places: Vec<Position>,
-    left_board: bool
+    left_board: bool,
 }
 
 impl Board {
@@ -32,14 +32,15 @@ impl Board {
         let guard: Position = lines
             .iter()
             .enumerate()
-            .map(|(row, x)| 
+            .map(|(row, x)| {
                 x.chars()
                     .enumerate()
                     .find(|(_, y)| y == &'^')
                     .map(|(col, _)| Position(row, col))
-                    
-            )
-            .flatten().next().unwrap();
+            })
+            .flatten()
+            .next()
+            .unwrap();
 
         Board {
             rows: lines.len() - 1,
@@ -48,50 +49,49 @@ impl Board {
             guard,
             guard_direction: Direction::North,
             guard_visited_places: vec![guard],
-            left_board: false
+            left_board: false,
         }
     }
 
     fn evaluate_board(mut self) -> Self {
-    let mut steps = 0; 
-      while !self.left_board && steps <= 10000 {
-        self.move_guard();
-        steps = steps + 1;
-      }
-      return self;
-
+        let mut steps = 0;
+        while !self.left_board && steps <= 10000 {
+            self.move_guard();
+            steps = steps + 1;
+        }
+        return self;
     }
 
     fn move_guard(&mut self) {
-    // Leaving the map?
-    if self.guard.0 == 0 && self.guard_direction == Direction::North
-    || self.guard.0 == self.rows && self.guard_direction == Direction::South
-    || self.guard.1 == self.columns && self.guard_direction == Direction::East
-    || self.guard.1 == 0 && self.guard_direction == Direction::West{
-            self.left_board = true;       
-    } else {
-    let next_position = match self.guard_direction {
-        Direction::North => Position(self.guard.0 - 1, self.guard.1),
-        Direction::South => Position(self.guard.0 + 1, self.guard.1),
-        Direction::East => Position(self.guard.0, self.guard.1 + 1),
-        Direction::West => Position(self.guard.0, self.guard.1 - 1),
-        Direction::None => self.guard
-    };
+        // Leaving the map?
+        if self.guard.0 == 0 && self.guard_direction == Direction::North
+            || self.guard.0 == self.rows && self.guard_direction == Direction::South
+            || self.guard.1 == self.columns && self.guard_direction == Direction::East
+            || self.guard.1 == 0 && self.guard_direction == Direction::West
+        {
+            self.left_board = true;
+        } else {
+            let next_position = match self.guard_direction {
+                Direction::North => Position(self.guard.0 - 1, self.guard.1),
+                Direction::South => Position(self.guard.0 + 1, self.guard.1),
+                Direction::East => Position(self.guard.0, self.guard.1 + 1),
+                Direction::West => Position(self.guard.0, self.guard.1 - 1),
+                Direction::None => self.guard,
+            };
 
-    if self.obstacles.contains(&next_position) {
-        self.guard_direction = match self.guard_direction{ 
-        Direction::North => Direction::East,
-        Direction::South => Direction::West,
-        Direction::East => Direction::South,
-        Direction::West => Direction::North,
-        Direction::None => Direction::None 
-        };
-    } else {
-
-    self.guard_visited_places.push(next_position.clone());
-    self.guard = next_position;
-    }
-    }
+            if self.obstacles.contains(&next_position) {
+                self.guard_direction = match self.guard_direction {
+                    Direction::North => Direction::East,
+                    Direction::South => Direction::West,
+                    Direction::East => Direction::South,
+                    Direction::West => Direction::North,
+                    Direction::None => Direction::None,
+                };
+            } else {
+                self.guard_visited_places.push(next_position.clone());
+                self.guard = next_position;
+            }
+        }
     }
 }
 
@@ -115,7 +115,7 @@ fn main() {
     result.guard_visited_places.sort();
     result.guard_visited_places.dedup();
     let count = result.guard_visited_places.len();
-    println!("{} unique Places", count); 
+    println!("{} unique Places", count);
     let (tx, rx): (Sender<bool>, Receiver<bool>) = channel();
     let mut children = Vec::new();
 
@@ -132,10 +132,13 @@ fn main() {
         children.push(handle);
     }
     let mut obstacle_results: Vec<bool> = Vec::new();
-    let mut count = 0; 
+    let mut count = 0;
     while count < result.guard_visited_places.len() {
         match rx.try_recv() {
-            Ok(x) => {obstacle_results.push(x); count += 1;},
+            Ok(x) => {
+                obstacle_results.push(x);
+                count += 1;
+            }
             Err(_) => (),
         }
     }

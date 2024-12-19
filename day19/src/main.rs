3adc8f8f68749a8765dcf6731527fbdb90907b79
bobredfox <sorlin@posteo.de>
2024-12-19 12:11:37 +0100

    Day 19 Teil 1

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
+use itertools::Itertools;
 
 type Towels = Vec<String>;
 type TowelPattern = Vec<String>;
+
 
 fn file_read(path: &str) -> (Towels, TowelPattern) {
     let file_content = fs::read_to_string(path).expect("File not found.");
@@ -11,7 +13,7 @@ fn file_read(path: &str) -> (Towels, TowelPattern) {
 
 fn parse_towels(input: &str) -> Towels {
     let mut towels: Towels = input.split(", ").map(|s| s.to_string()).collect();
-    towels.sort_by(|a,b| b.len().cmp(&a.len()));
+    towels.sort_by(|a, b| b.len().cmp(&a.len()));
     return towels;
 }
 
@@ -19,31 +21,61 @@ fn parse_pattern(input: &str) -> TowelPattern {
     input.lines().map(|s| s.to_string()).collect()
 }
 
-fn pattern_can_be_made(pattern: &str, towels: &Vec<String>) -> bool{
-    
-    let mut combined = String::from("");
-    while towels.iter().any(|t| add_towel(pattern, t, &mut combined)){
-    };
-    return combined == pattern;
+
+fn can_pattern_be_made(pattern: &str, towels: &Vec<String>) -> bool {
+    let mut words: Vec<String> = vec![String::from("")];
+
+    while words.iter().any(|w| w.len() < pattern.len()) && !words.iter().any(|w| w.as_str() == pattern) {
+        words = compare_each_word(pattern, towels, words);
+    }
+
+    return words.iter().any(|w| w.as_str() == pattern);
 }
 
-fn add_towel(pattern: &str, towel: &String, combined: &mut String) -> bool {
-    let test = combined.clone() + towel;
-    if test.len() > pattern.len() {
-        return false;
+
+fn can_pattern_be_made_how(pattern: &str, towels: &Vec<String>) -> usize {
+    let mut words: Vec<String> = vec![String::from("")];
+    let mut count = 0;
+    while words.iter().any(|w| w.len() < pattern.len()) && words.len() > 0 {
+        let temp = compare_each_word_double(pattern, towels, words);
+        println!("Teste...");
+        words = temp.iter().filter(|w| w.as_str() != pattern).map(|s| s.to_owned()).collect();
+        count += temp.len() - words.len();
+    }
+
+    return count;
+}
+
+
+fn compare_each_word(pattern: &str, towels: &Vec<String>, words: Vec<String>) -> Vec<String> {
+    words.iter().flat_map(|w| does_word(pattern, towels, w.as_str())).unique().collect()
+}
+
+fn compare_each_word_double(pattern: &str, towels: &Vec<String>, words: Vec<String>) -> Vec<String> {
+    words.iter().flat_map(|w| does_word(pattern, towels, w.as_str())).collect()
+}
+
+fn does_word(pattern: &str, towels: &Vec<String>, word: &str) -> Vec<String> {
+    towels.iter()
+    .filter(|s| does_it_fit(pattern, s.as_str(), word))
+    .map(|t| word.to_owned() + t).collect()
+}
+
+
+fn does_it_fit(pattern: &str, towel: &str, word: &str) -> bool {
+    let new_word = word.to_owned() + towel;
+    if new_word.len() <= pattern.len() {
+        return pattern[..new_word.len()] == new_word;
     } else {
-    let slice = &pattern[..test.len()];
-    match test.as_str() {
-        s if s == slice => {combined.push_str(&towel);
-            return true },
-        _ => false,
+        return false;
     }
 }
-}
+
 
 fn main() {
     let (towels, patterns) = file_read("input.txt");
-    let possible: Vec<bool> = patterns.iter().map(|p| pattern_can_be_made(p, &towels)).collect();
-    let possible_solutions = possible.iter().filter(|x| **x).count();
-    println!("Towels: {:?}", possible_solutions);
+    // let test: Vec<bool> = patterns.iter().map(|p| can_pattern_be_made(p.as_str(), &towels)).collect();
+    let test_count: Vec<usize> = patterns.iter().enumerate().inspect(|(index, s)| println!("Processing {} of {}", index, patterns.len())).map(|(_, p)| can_pattern_be_made_how(p.as_str(), &towels)).collect();
+    let numbers: usize = test_count.iter().sum();
+    println!("Patterns: {}", numbers);
 }
